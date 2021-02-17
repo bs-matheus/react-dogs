b03925f29be693c81408a3692e28cedc7f156a76
Matheus Barbosa <matheusbs.silva@outlook.com>
2021-02-16 22:04:44 -0300

    LoginForm improved

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import styles from './Login.module.css';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
+import { UserContext } from '../../UserContext';
 
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to="/account" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/create" element={<LoginCreate />} />
-        <Route path="/lost" element={<LoginPasswordLost />} />
-        <Route path="/reset" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/create" element={<LoginCreate />} />
+          <Route path="/lost" element={<LoginPasswordLost />} />
+          <Route path="/reset" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
